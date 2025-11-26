f79ff98e2726bc657b9d626f43e26ebb84020807
Daniel Amado <daniel.amadove@gmail.com>
2025-11-26 18:34:04 -0500

    feat: enhance CriteriosEligibilidad data structure with additional cardiovascular criteria and improved value types

--- src/data/CriteriosEligibilidad.ts
+++ src/data/CriteriosEligibilidad.ts
@@ -1,10 +1,10 @@
 type CriteriaValue = {
-  AOC: number | "N";
-  IM: number | "N";
-  AVC: number | "N";
-  PSP: number | "N";
-  ISP: number | "N";
-  IMP: number | "N";
+  AOC: number | "N" | { I: number; C: number };
+  IM: number | "N" | { I: number; C: number };
+  AVC: number | "N" | { I: number; C: number };
+  PSP: number | "N" | { I: number; C: number };
+  ISP: number | "N" | { I: number; C: number };
+  IMP: number | "N" | { I: number; C: number };
 };
 type CriteriaNode = {
   label: string;
@@ -25,25 +25,25 @@ export const criterios: CriteriaNode[] = [
         children: [
           {
             label: "De la menarquia a <40 años",
-            values: { AOC: 1, IM: 1, AVC: 1, PSP: 'N', ISP: 'N', IMP: 'N' }
+            values: { AOC: 1, IM: 1, AVC: 1, PSP: "N", ISP: "N", IMP: "N" },
           },
           {
             label: "De la menarquia a <18 años",
-            values: { AOC: 'N', IM: 'N', AVC: 'N', PSP: 1, ISP: 2, IMP: 1 }
+            values: { AOC: "N", IM: "N", AVC: "N", PSP: 1, ISP: 2, IMP: 1 },
           },
           {
             label: "≥40 años",
-            values: { AOC: 2, IM: 2, AVC: 2, PSP: 'N', ISP: 'N', IMP: 'N' }
+            values: { AOC: 2, IM: 2, AVC: 2, PSP: "N", ISP: "N", IMP: "N" },
           },
           {
             label: "18 a 45 años",
-            values: { AOC: 'N', IM: 'N', AVC: 'N', PSP: 1, ISP: 1, IMP: 1 }
+            values: { AOC: "N", IM: "N", AVC: "N", PSP: 1, ISP: 1, IMP: 1 },
           },
           {
             label: ">45 años",
-            values: { AOC: 'N', IM: 'N', AVC: 'N', PSP: 1, ISP: 2, IMP: 1 }
-          }
-        ]
+            values: { AOC: "N", IM: "N", AVC: "N", PSP: 1, ISP: 2, IMP: 1 },
+          },
+        ],
       },
       {
         label: "Número de partos",
@@ -51,13 +51,13 @@ export const criterios: CriteriaNode[] = [
         children: [
           {
             label: "No ha tenido partos (nulípara)",
-            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 }
+            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 },
           },
           {
             label: "Ha tenido uno o más partos",
-            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 }
+            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 },
           },
-        ]
+        ],
       },
       {
         label: "Lactancia materna",
@@ -65,17 +65,18 @@ export const criterios: CriteriaNode[] = [
         children: [
           {
             label: "<6 semanas después del parto",
-            values: { AOC: 4, IM: 4, AVC: 4, PSP: 2, ISP: 3, IMP: 2 }
+            values: { AOC: 4, IM: 4, AVC: 4, PSP: 2, ISP: 3, IMP: 2 },
           },
           {
-            label: "≥6 semanas a <6 meses después del parto (básicamente amamantando)",
-            values: { AOC: 3, IM: 3, AVC: 3, PSP: 1, ISP: 1, IMP: 1 }
+            label:
+              "≥6 semanas a <6 meses después del parto (básicamente amamantando)",
+            values: { AOC: 3, IM: 3, AVC: 3, PSP: 1, ISP: 1, IMP: 1 },
           },
           {
             label: "≥≥6 meses después del parto",
-            values: { AOC: 2, IM: 2, AVC: 2, PSP: 1, ISP: 1, IMP: 1 }
-          }
-        ]
+            values: { AOC: 2, IM: 2, AVC: 2, PSP: 1, ISP: 1, IMP: 1 },
+          },
+        ],
       },
       {
         label: "Posparto (mujeres que no amamantan)",
@@ -83,25 +84,27 @@ export const criterios: CriteriaNode[] = [
         children: [
           {
             label: "<21 días",
-            values: { AOC: 3, IM: 3, AVC: 3, PSP: 1, ISP: 1, IMP: 1 }
+            values: { AOC: 3, IM: 3, AVC: 3, PSP: 1, ISP: 1, IMP: 1 },
           },
           {
-            label: "<21 dias Con otros factores adicionales de riesgo de tromboembolia venosa",
-            values: { AOC: 4, IM: 4, AVC: 4, PSP: 'N', ISP: 'N', IMP: 'N' }
+            label:
+              "<21 dias Con otros factores adicionales de riesgo de tromboembolia venosa",
+            values: { AOC: 4, IM: 4, AVC: 4, PSP: "N", ISP: "N", IMP: "N" },
           },
           {
             label: "21 a 42 días",
-            values: { AOC: 2, IM: 2, AVC: 2, PSP: 1, ISP: 1, IMP: 1 }
+            values: { AOC: 2, IM: 2, AVC: 2, PSP: 1, ISP: 1, IMP: 1 },
           },
           {
-            label: "21 a 42 días Con otros factores adicionales de riesgo de tromboembolia venosa",
-            values: { AOC: 3, IM: 3, AVC: 3, PSP: 'N', ISP: 'N', IMP: 'N' }
+            label:
+              "21 a 42 días Con otros factores adicionales de riesgo de tromboembolia venosa",
+            values: { AOC: 3, IM: 3, AVC: 3, PSP: "N", ISP: "N", IMP: "N" },
           },
           {
             label: ">42 días",
-            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 }
-          }
-        ]
+            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 },
+          },
+        ],
       },
       {
         label: "Después de un aborto",
@@ -109,17 +112,17 @@ export const criterios: CriteriaNode[] = [
         children: [
           {
             label: "En el primer trimestre",
-            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 }
+            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 },
           },
           {
             label: "En el segundo trimestre",
-            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 }
+            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 },
           },
           {
             label: "inmediatamente después de un aborto séptico",
-            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 }
-          }
-        ]
+            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 },
+          },
+        ],
       },
       {
         label: "Afección o circunstancia",
@@ -127,13 +130,13 @@ export const criterios: CriteriaNode[] = [
         children: [
           {
             label: "Antecedentes de embarazo ectópico",
-            values: { AOC: 1, IM: 1, AVC: 1, PSP: 2, ISP: 1, IMP: 1 }
+            values: { AOC: 1, IM: 1, AVC: 1, PSP: 2, ISP: 1, IMP: 1 },
           },
           {
             label: "Antecedentes de cirugía pélvica",
-            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 }
-          }
-        ]
+            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 },
+          },
+        ],
       },
       {
         label: "Tabaquismo",
@@ -141,7 +144,7 @@ export const criterios: CriteriaNode[] = [
         children: [
           {
             label: "Edad <35 años",
-            values: { AOC: 2, IM: 2, AVC: 2, PSP: 1, ISP: 1, IMP: 1 }
+            values: { AOC: 2, IM: 2, AVC: 2, PSP: 1, ISP: 1, IMP: 1 },
           },
           {
             label: "Edad ≥35 años",
@@ -149,38 +152,40 @@ export const criterios: CriteriaNode[] = [
             children: [
               {
                 label: "<15 cigarrillos al día",
-                values: { AOC: 3, IM: 2, AVC: 3, PSP: 1, ISP: 1, IMP: 1 }
+                values: { AOC: 3, IM: 2, AVC: 3, PSP: 1, ISP: 1, IMP: 1 },
               },
               {
                 label: "≥15 cigarrillos al día",
-                values: { AOC: 4, IM: 3, AVC: 4, PSP: 1, ISP: 1, IMP: 1 }
-              }
-            ]
-          }
-        ]
+                values: { AOC: 4, IM: 3, AVC: 4, PSP: 1, ISP: 1, IMP: 1 },
+              },
+            ],
+          },
+        ],
       },
       {
         label: "Obesidad",
         open: false,
-        children: [{
-          label: "Índice de masa corporal ≥30 kg/m2",
-          values: { AOC: 2, IM: 2, AVC: 2, PSP: 1, ISP: 1, IMP: 1 }
-        },
-        {
-          label: "Índice de masa corporal <30 kg/m2",
-          values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 }
-        }
-      ]
-      }
-    ]
+        children: [
+          {
+            label: "Índice de masa corporal ≥30 kg/m2",
+            values: { AOC: 2, IM: 2, AVC: 2, PSP: 1, ISP: 1, IMP: 1 },
+          },
+          {
+            label: "Índice de masa corporal <30 kg/m2",
+            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 },
+          },
+        ],
+      },
+    ],
   },
   {
     label: "TRASTORNOS CARDIOVASCULARES",
     open: false,
     children: [
       {
-        label: "Múltiples factores de riesgo de enfermedad cardivascular arterial (mayor edad, tabaquismo, diabetes e hipertensión)",
-        values: { AOC: 4, IM: 4, AVC: 4, PSP: 2, ISP: 3, IMP: 2 }
+        label:
+          "Múltiples factores de riesgo de enfermedad cardivascular arterial (mayor edad, tabaquismo, diabetes e hipertensión)",
+        values: { AOC: 4, IM: 4, AVC: 4, PSP: 2, ISP: 3, IMP: 2 },
       },
       {
         label: "Hipertensión",
@@ -189,12 +194,12 @@ export const criterios: CriteriaNode[] = [
           {
             label:
               "Antecedentes de hipertensión, cuando NO se puede evaluar la tensión arterial (incluida la hipertensión en el embarazo)",
-            values: { AOC: 3, IM: 3, AVC: 3, PSP: 2, ISP: 2, IMP: 2 }
+            values: { AOC: 3, IM: 3, AVC: 3, PSP: 2, ISP: 2, IMP: 2 },
           },
           {
             label:
               "Hipertensión adecuadamente controlada, cuando SE PUEDE evaluar la tensión arterial",
-            values: { AOC: 3, IM: 3, AVC: 3, PSP: 1, ISP: 2, IMP: 1 }
+            values: { AOC: 3, IM: 3, AVC: 3, PSP: 1, ISP: 2, IMP: 1 },
           },
           {
             label: "Tensión arterial elevada (medida de la manera adecuada)",
@@ -202,16 +207,119 @@ export const criterios: CriteriaNode[] = [
             children: [
               {
                 label: "Sistólica 140-159 mm Hg o diastólica 90-99 mm Hg",
-                values: { AOC: 3, IM: 3, AVC: 3, PSP: 1, ISP: 2, IMP: 1 }
+                values: { AOC: 3, IM: 3, AVC: 3, PSP: 1, ISP: 2, IMP: 1 },
               },
               {
                 label: "Sistólica >=160 mm Hg o diastólica >=100 mm Hg",
-                values: { AOC: 4, IM: 4, AVC: 4, PSP: 2, ISP: 3, IMP: 2 }
-              }
-            ]
+                values: { AOC: 4, IM: 4, AVC: 4, PSP: 2, ISP: 3, IMP: 2 },
+              },
+            ],
+          },
+          {
+            label: "Enfermedad vascular",
+            values: { AOC: 4, IM: 4, AVC: 4, PSP: 2, ISP: 3, IMP: 2 },
           }
-        ]
-      }
-    ]
-  }
+        ],
+      },
+      {
+        label:
+          "Antecedentes de hipertensión en el embarazo (cuando la tensión arterial actual puede medirse y es normal)",
+        values: { AOC: 2, IM: 2, AVC: 2, PSP: 1, ISP: 1, IMP: 1 },
+      },
+      {
+        label: "Trombosis venosa profunda (TVP)/ Embolia pulmonar (EP)",
+        open: false,
+        children: [
+          {
+            label: "Antecedentes de TVP/EP",
+            values: { AOC: 4, IM: 4, AVC: 4, PSP: 2, ISP: 2, IMP: 2 },
+          },
+          {
+            label: "TVP/EP aguda",
+            values: { AOC: 4, IM: 4, AVC: 4, PSP: 3, ISP: 3, IMP: 3 },
+          },
+          {
+            label: "TVP/EP y con tratamiento anticoagulante",
+            values: { AOC: 4, IM: 4, AVC: 4, PSP: 2, ISP: 2, IMP: 2 },
+          },
+          {
+            label:
+              "Antecedentes familiares de TVP/EP (parientes de primer grado)",
+            values: { AOC: 2, IM: 2, AVC: 2, PSP: 1, ISP: 1, IMP: 1 },
+          },
+          {
+            label: "Cirugía mayor",
+            open: false,
+            children: [
+              {
+                label: "Con inmovilización prolongada",
+                values: { AOC: 4, IM: 4, AVC: 4, PSP: 2, ISP: 2, IMP: 2 },
+              },
+              {
+                label: "Sin inmovilización prolongada",
+                values: { AOC: 2, IM: 2, AVC: 2, PSP: 1, ISP: 1, IMP: 1 },
+              },
+            ],
+          },
+          {
+            label: "Cirugía menor sin inmovilización prolongada",
+            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 },
+          },
+        ],
+      },
+      {
+        label:
+          "Mutaciones trombogénicas conocidas (p. ej., factor V de Leiden, mutación de la protrombina; deficiencias de proteína S, proteína C y antitrombina)",
+        values: { AOC: 4, IM: 4, AVC: 4, PSP: 2, ISP: 2, IMP: 2 },
+      },
+      {
+        label: "Trombosis venosa superficial",
+        open: false,
+        children: [
+          {
+            label: "Venas varicosas (várices)",
+            values: { AOC: 1, IM: 1, AVC: 1, PSP: 1, ISP: 1, IMP: 1 },
+          },
+          {
+            label: "Trombosis venosa superficial",
+            values: { AOC: 2, IM: 2, AVC: 2, PSP: 1, ISP: 1, IMP: 1 },
+          },
+          {
+            label: "Cardiopatía isquémica",
+            open: false,
+            children: [
+              {
+                label: "En curso",
+                values: {
+                  AOC: 4,
+                  IM: 4,
+                  AVC: 4,
+                  PSP: { I: 2, C: 3 },
+                  ISP: 3,
+                  IMP: { I: 2, C: 3 },
+                },
+              },
+              {
+                label: "Antecedentes",
+                values: {
+                  AOC: 4,
+                  IM: 4,
+                  AVC: 4,
+                  PSP: { I: 2, C: 3 },
+                  ISP: 3,
+                  IMP: { I: 2, C: 3 },
+                },
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label:
+          "Accidente cerebrovascular (antecedentes de accidente cerebrovascular)",
+        values: { AOC: 2, IM: 2, AVC: 2, PSP: 2, ISP: 2, IMP: 2 },
+      },
+      
+    ],
+  },
 ];
