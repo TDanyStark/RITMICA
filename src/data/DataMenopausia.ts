61819d200eb1b37394b1c01eefa4a3cb5a98c4b3
Daniel Amado <daniel.amadove@gmail.com>
2025-12-04 12:45:33 -0500

    feat: add condicionesMenopausia data structure for menopause criteria

---
+++ src/data/DataMenopausia.ts
@@ -0,0 +1,39 @@
+export const condicionesMenopausia = [
+  {
+    label: "EDAD Y DURACION",
+    children: [
+      {
+        label: "Edad",
+        children: [
+          {
+            label: "<40 años",
+            children: [
+              {
+                label: "<5 años",
+                values: {
+                  "THM combinada oral": 1,
+                  "THM combinada trans": 1,
+                  "THM solo estrógeno oral": 1,
+                  "THM solo estrógeno trans": 1,
+                  "Tibolona": "NA",
+                  "THM local": 1,
+                },
+              },
+              {
+                label: ">=5 años",
+                values: {
+                  "THM combinada oral": 1,
+                  "THM combinada trans": 1,
+                  "THM solo estrógeno oral": 1,
+                  "THM solo estrógeno trans": 1,
+                  "Tibolona": "NA",
+                  "THM local": 1,
+                },
+              }
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
