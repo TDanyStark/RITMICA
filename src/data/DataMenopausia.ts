69a2f9df9c2ee33e6ca4a92dc9a9dd9ff6b3dbbc
Daniel Amado <daniel.amadove@gmail.com>
2025-12-04 12:53:39 -0500

    feat: expand condicionesMenopausia data structure with detailed age categories and values

--- src/data/DataMenopausia.ts
+++ src/data/DataMenopausia.ts
@@ -12,18 +12,243 @@ export const condicionesMenopausia = [
               "THM combinada trans": 1,
               "THM solo estrógeno oral": 1,
               "THM solo estrógeno trans": 1,
-              "Tibolona": "NA",
+              Tibolona: "NA",
               "THM local": 1,
             },
           },
           {
-            label: ">=5 años",
+            label: ">5 años",
             values: {
               "THM combinada oral": 1,
               "THM combinada trans": 1,
               "THM solo estrógeno oral": 1,
               "THM solo estrógeno trans": 1,
-              "Tibolona": "NA",
+              Tibolona: "NA",
+              "THM local": 1,
+            },
+          },
+        ],
+      },
+      {
+        label: "40-44",
+        children: [
+          {
+            label: "<5 años",
+            values: {
+              "THM combinada oral": 2,
+              "THM combinada trans": 1,
+              "THM solo estrógeno oral": 2,
+              "THM solo estrógeno trans": 1,
+              Tibolona: 2,
+              "THM local": 1,
+            },
+          },
+          {
+            label: ">5 años",
+            values: {
+              "THM combinada oral": 2,
+              "THM combinada trans": 2,
+              "THM solo estrógeno oral": 2,
+              "THM solo estrógeno trans": 2,
+              Tibolona: 2,
+              "THM local": 1,
+            },
+          },
+        ],
+      },
+      {
+        label: "45-49",
+        children: [
+          {
+            label: "<5 años",
+            values: {
+              "THM combinada oral": 2,
+              "THM combinada trans": 2,
+              "THM solo estrógeno oral": 2,
+              "THM solo estrógeno trans": 1,
+              Tibolona: 2,
+              "THM local": 1,
+            },
+          },
+          {
+            label: ">5 años",
+            values: {
+              "THM combinada oral": 2,
+              "THM combinada trans": 2,
+              "THM solo estrógeno oral": 2,
+              "THM solo estrógeno trans": 2,
+              Tibolona: 2,
+              "THM local": 1,
+            },
+          },
+        ],
+      },
+      {
+        label: "50-59",
+        children: [
+          {
+            label: "<5 años",
+            values: {
+              "THM combinada oral": 2,
+              "THM combinada trans": 2,
+              "THM solo estrógeno oral": 2,
+              "THM solo estrógeno trans": 2,
+              Tibolona: {
+                C:{
+                  val: 2,
+                  condition: "<55"
+                },
+                I:{
+                  val: 1,
+                  condition: ">55"
+                }
+              },
+              "THM local": 1,
+            },
+          },
+          {
+            label: ">5 años",
+            values: {
+              "THM combinada oral": 2,
+              "THM combinada trans": 2,
+              "THM solo estrógeno oral": 2,
+              "THM solo estrógeno trans": 2,
+              Tibolona: {
+                C:{
+                  val: 2,
+                  condition: "<55"
+                },
+                I:{
+                  val: 1,
+                  condition: ">55"
+                }
+              },
+              "THM local": 1,
+            },
+          },
+        ],
+      },
+      {
+        label: "60-69",
+        children: [
+          {
+            label: "<5 años",
+            values: {
+              "THM combinada oral": {
+                C:{
+                  val: 2,
+                  condition: "(continuación de la THM)"
+                },
+                I:{
+                  val: 3,
+                  condition: "(inicio de la THM y >10 años de menopausia)"
+                }
+              },
+              "THM combinada trans":{
+                C:{
+                  val: 2,
+                  condition: "(continuación de la THM)"
+                },
+                I:{
+                  val: 3,
+                  condition: "(inicio de la THM y >10 años de menopausia)"
+                }
+              },
+              "THM solo estrógeno oral":{
+                C:{
+                  val: 2,
+                  condition: "(continuación de la THM)"
+                },
+                I:{
+                  val: 3,
+                  condition: "(inicio de la THM y >10 años de menopausia)"
+                }
+              },
+              "THM solo estrógeno trans":{
+                C:{
+                  val: 2,
+                  condition: "(continuación de la THM)"
+                },
+                I:{
+                  val: 3,
+                  condition: "(inicio de la THM y >10 años de menopausia)"
+                }
+              },
+              Tibolona: 2,
+              "THM local": 1,
+            },
+          },
+          {
+            label: ">5 años",
+            values: {
+              "THM combinada oral": {
+                C:{
+                  val: 2,
+                  condition: "(continuación de la THM)"
+                },
+                I:{
+                  val: 3,
+                  condition: "(inicio de la THM y >10 años de menopausia)"
+                }
+              },
+              "THM combinada trans":{
+                C:{
+                  val: 2,
+                  condition: "(continuación de la THM)"
+                },
+                I:{
+                  val: 3,
+                  condition: "(inicio de la THM y >10 años de menopausia)"
+                }
+              },
+              "THM solo estrógeno oral":{
+                C:{
+                  val: 2,
+                  condition: "(continuación de la THM)"
+                },
+                I:{
+                  val: 3,
+                  condition: "(inicio de la THM y >10 años de menopausia)"
+                }
+              },
+              "THM solo estrógeno trans":{
+                C:{
+                  val: 2,
+                  condition: "(continuación de la THM)"
+                },
+                I:{
+                  val: 3,
+                  condition: "(inicio de la THM y >10 años de menopausia)"
+                }
+              },
+              Tibolona: 2,
+              "THM local": 1,
+            },
+          },
+        ],
+      },
+      {
+        label: ">70",
+        children: [
+          {
+            label: "<5 años",
+            values: {
+              "THM combinada oral": 3,
+              "THM combinada trans": 3,
+              "THM solo estrógeno oral": 3,
+              "THM solo estrógeno trans": 3,
+              Tibolona: 2,
+              "THM local": 1,
+            },
+          },
+          {
+            label: ">5 años",
+            values: {
+              "THM combinada oral": 3,
+              "THM combinada trans": 3,
+              "THM solo estrógeno oral": 3,
+              "THM solo estrógeno trans": 3,
+              Tibolona: 3,
               "THM local": 1,
             },
           },
